1f7a8b239d5b3101f3c5c045e02924cab65f89e3
Tushar K <ktusharhk@gmail.com>
2020-07-07 16:08:11 +1000

    fix to mongo URL

--- public/db.js
+++ public/db.js
@@ -2,28 +2,31 @@ let db;
 // create a new db request for a "budget" database.
 const request = indexedDB.open("budget", 1);
 
-request.onupgradeneeded = function(event) {
-   // create object store called "pending" and set autoIncrement to true
+request.onupgradeneeded = function (event) {
+  // create object store called "pending" and set autoIncrement to true
   const db = event.target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+  db.createObjectStore("pending", {
+    autoIncrement: true
+  });
 };
 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
   db = event.target.result;
-
-  // check if app is online before reading from db
+  // navigator.onLine checks the browser's online status
+  // https://developer.mozilla.org/en-US/docs/Web/API/NavigatorOnLine/onLine
+  // Check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
 };
 
-request.onerror = function(event) {
+request.onerror = function (event) {
   console.log("Woops! " + event.target.errorCode);
 };
 
 function saveRecord(record) {
   //alert("saveRecord in db.js file");
-    // create a transaction on the pending db with readwrite access
+  // create a transaction on the pending db with readwrite access
   const transaction = db.transaction(["pending"], "readwrite");
 
   // access your pending object store
@@ -41,27 +44,27 @@ function checkDatabase() {
   // get all records from store and set to a variable
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your pending db
-        const transaction = db.transaction(["pending"], "readwrite");
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["pending"], "readwrite");
 
-        // access your pending object store
-        const store = transaction.objectStore("pending");
+          // access your pending object store
+          const store = transaction.objectStore("pending");
 
-        // clear all items in your store
-        store.clear();
-      });
+          // clear all items in your store
+          store.clear();
+        });
     }
   };
 }
--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  { useNewUrlParser: true }
+);
 
 // routes
 app.use(require("./routes/api.js"));
